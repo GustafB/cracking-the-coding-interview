2f17b3994b261e7bac3f01dfe7f68e48f306d3c7
GustafN <gustaf.brostedt@gmail.com>
2018-03-07 09:04:46 -0500

    fix bug

--- arrays-and-strings/checkPermutation_1-2.js
+++ arrays-and-strings/checkPermutation_1-2.js
@@ -9,17 +9,3 @@ const checkPermutation = (strA, strB) => {
 const sortString = (string) => {
   return string.split('').sort((a, b) => a.localeCompare(b)).join('');
 }
-
-
-//#### ASCII SUM #### (O(n + m))
-
-const checkPermutation = (strA, strB) => {
-  if (strA.length !== strB.length) return false;
-  return asciiSum(strA) === asciiSum(strB);
-}
-
-const asciiSum = (string) => {
-  return string.split('').reduce((a, b) => {
-    return a + b.charCodeAt(0);
-  }, 0);
-}
