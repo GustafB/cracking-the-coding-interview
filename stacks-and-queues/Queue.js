504e9f60a8fc72aec49057d0e933c2d104f202d9
GustafN <gustaf.brostedt@gmail.com>
2018-03-11 19:10:47 -0400

    implement queue

--- stacks-and-queues/Queue.js
+++ stacks-and-queues/Queue.js
@@ -1,8 +1,29 @@
 const Queue = function() {
   this.storage = [];
   this.size = 0;
+  this.first = 0;
 };
 
 Queue.prototype.enqueue = function(data) {
-  this.storage
+  this.storage[this.size++] = data;
+  console.log(this.storage)
 }
+
+Queue.prototype.dequeue = function() {
+  let data = null;
+  if (this.size) {
+    data = this.storage[this.first];
+    this.storage[this.first++] = null;
+    this.size--;
+  }
+  return data;
+}
+
+Queue.prototype.peek = function() {
+  return this.storage[this.first];
+}
+
+Queue.prototype.isEmpty = function() {
+  return this.size === 0;
+}
+
